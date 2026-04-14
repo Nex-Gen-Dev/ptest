9e47c16bacb44f13929a5b6827782977d3304839
Nex-Gen-Dev <sgmatzev23@gmail.com>
2026-04-14 14:15:30 -0400

    Add poll option input and submission handler

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function searchStores() {
-    let input = document.getElementById('storeSearch').value.toLowerCase();
-    let storeItems = document.getElementsByClassName('store-item');
-
-    for (let i = 0; i < storeItems.length; i++) {
-        let text = storeItems[i].textContent || storeItems[i].innerText;
-        if (text.toLowerCase().indexOf(input) > -1) {
-            storeItems[i].style.display = "";
-        } else {
-            storeItems[i].style.display = "none";
-        }
-    }
+function addOption() {
+    const container = document.getElementById('optionsList');
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.className = 'poll-option';
+    input.placeholder = 'New Option';
+    input.style.marginTop = "10px";
+    container.appendChild(input);
 }
+
+// Function to handle form "submission" (visual only for now)
+document.querySelector('.poll-form button.btn').addEventListener('click', function() {
+    const title = document.getElementById('pollTitle').value;
+    if(title) {
+        alert("Poll '" + title + "' has been created (Draft Mode)!");
+    } else {
+        alert("Please enter a Poll Title.");
+    }
+});
